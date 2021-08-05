9b07c419a69756f6b77505dc1d2ce05466b97824
RicardoYoshio <rica.tomokane@gmail.com>
2021-08-05 20:23:28 -0300

    Product

---
+++ src/main/java/com/course/curso/repositories/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.course.curso.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.course.curso.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long>{
+
+
+}
