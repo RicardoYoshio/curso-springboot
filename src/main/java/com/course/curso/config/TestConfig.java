ab55639c81c929151ed71ab3c28ddb70c35d4a8b
RicardoYoshio <rica.tomokane@gmail.com>
2021-08-07 18:13:10 -0300

    Subtotal & Total methods

--- src/main/java/com/course/curso/config/TestConfig.java
+++ src/main/java/com/course/curso/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.course.curso.entities.Category;
 import com.course.curso.entities.Order;
 import com.course.curso.entities.OrderItem;
+import com.course.curso.entities.Payment;
 import com.course.curso.entities.Product;
 import com.course.curso.entities.User;
 import com.course.curso.entities.enums.OrderStatus;
@@ -83,5 +84,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
+		
 	}
 }
